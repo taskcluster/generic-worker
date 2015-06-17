eb8767a06a00069960c23d0fe037f8a2ca78d30c
Pete Moore <pmoore@mozilla.com>
2015-06-17 15:32:05 +0200

    Work in progress...

--- powershell_windows.go
+++ powershell_windows.go
@@ -10,15 +10,14 @@ import (
 // the working directory to run from. Returns an error if there is a problem
 // creating the script.
 func createRunAsUserScript(filename string) error {
-	scriptContents := `$username = $args[0]\r
-$password = $args[1]\r
-$script = $args[2]\r
-$dir = $args[3]\r
-\r
-$credentials = New-Object System.Management.Automation.PSCredential -ArgumentList @($username,(ConvertTo-SecureString -String $password -A\r
-sPlainText -Force))\r
-\r
-Start-Process $script -WorkingDirectory $dir -Credential ($credentials)\r
-`
+	scriptContents := "$username = $args[0]\r\n"
+	scriptContents += "$password = $args[1]\r\n"
+	scriptContents += "$script = $args[2]\r\n"
+	scriptContents += "$dir = $args[3]\r\n"
+	scriptContents += "\r\n"
+	scriptContents += "$credentials = New-Object System.Management.Automation.PSCredential -ArgumentList @($username,(ConvertTo-SecureString -String $password -A\r\n"
+	scriptContents += "sPlainText -Force))\r\n"
+	scriptContents += "\r\n"
+	scriptContents += "Start-Process $script -WorkingDirectory $dir -Credential ($credentials)\r\n"
 	return ioutil.WriteFile(filename, []byte(scriptContents), 0755)
 }
