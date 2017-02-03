f17dc0c6487c5ff9089bf1453a592e68303d6e8f
Pete Moore <pmoore@mozilla.com>
2017-02-03 19:26:30 +0100

    try again

--- user_creation_windows_test.go
+++ user_creation_windows_test.go
@@ -11,13 +11,9 @@ func TestRunAfterUserCreation(t *testing.T) {
 	if config.RunTasksAsCurrentUser {
 		t.Skip("Skipping since running as current user...")
 	}
-	config.RunAfterUserCreation = []string{
-		"C:\\Windows\\System32\\cmd.exe",
-		"/c",
-		"echo hello > %USERPROFILE%\run-after-user",
-	}
+	config.RunAfterUserCreation = filepath.Join(cwd, "testdata", "run-after-user.bat")
 	PrepareTaskEnvironment()
-	file := filepath.Join(taskContext.TaskDir, "run-after-user")
+	file := filepath.Join(taskContext.TaskDir, "run-after-user.txt")
 	_, err := os.Stat(file)
 	if err != nil {
 		t.Fatal("Got error when looking for file %v: %v", file, err)
