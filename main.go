802952d08a75d20858cf478fc5e90370b0186b4e
Chinmay Kousik <chinmaykousik1@gmail.com>
2017-10-20 02:12:15 +0530

    Webhooktunnel integration

--- main.go
+++ main.go
@@ -606,11 +606,6 @@ func RunWorker() (exitCode ExitCode) {
 	TunnelServer = getWebhookServer(creds)
 	if TunnelServer != nil {
 		TunnelServer.Initialise()
-		Features = append(Features, &WebhookLogFeature{})
-	} else {
-		// fallback to using the livelog binary in case the
-		// the TunnelServer cannot be instantiated.
-		Features = append(Features, &LiveLogFeature{})
 	}
 
 	err = initialiseFeatures()
