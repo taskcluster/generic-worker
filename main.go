0427f0b73f378636c3c475f6d1422f8f9c10172f
Pete Moore <pmoore@mozilla.com>
2018-01-10 12:59:28 +0100

    Version bump from 10.4.1 to 10.5.0alpha1

--- main.go
+++ main.go
@@ -47,7 +47,7 @@ var (
 
 	logName = "public/logs/live_backing.log"
 
-	version  = "10.4.1"
+	version  = "10.5.0alpha1"
 	revision = "" // this is set during build with `-ldflags "-X main.revision=$(git rev-parse HEAD)"`
 	usage    = `
 generic-worker
