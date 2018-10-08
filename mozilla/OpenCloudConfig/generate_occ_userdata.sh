f9eada58d11b9dc0eec0969c065d9a5981776b46
Pete Moore <pmoore@mozilla.com>
2018-10-08 13:01:34 +0200

    wip

--- mozilla/OpenCloudConfig/generate_occ_userdata.sh
+++ mozilla/OpenCloudConfig/generate_occ_userdata.sh
@@ -1,8 +1,15 @@
 #!/bin/bash -e
-cd "$(dirname "${0}")"
+THIS_SCRIPT_DIR="$(dirname "${0}")"
+
+# Default directory to look for definitions is current directory.
+# To select a different directory, simply export WORKER_TYPES_DIR
+# to chosen directory before calling this script.
+export WORKER_TYPES_DIR=${WORKER_TYPES_DIR:-.}
+
+go install ./transform-occ
 
 echo "Removing..."
-rm -v gecko-*/userdata
+rm -v "${WORKER_TYPES_DIR}"/gecko-*/userdata
 
 echo "Generating..."
-curl -L 'https://github.com/mozilla-releng/OpenCloudConfig/tree/master/userdata/Manifest' 2>/dev/null | sed -n 's/.*\(gecko[^.]*\)\.json.*/\1/p' | sort -u | xargs -n 1 -P 32 ./transform.sh
+curl -L 'https://github.com/mozilla-releng/OpenCloudConfig/tree/master/userdata/Manifest' 2>/dev/null | sed -n 's/.*\(gecko[^.]*\)\.json.*/\1/p' | sort -u | xargs -n 1 -P 32 "${THIS_SCRIPT_DIR}/transform.sh"
