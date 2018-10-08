becd6c07ce14f7e1f68c55b1ecded72f391ac71f
Pete Moore <pmoore@mozilla.com>
2018-10-08 13:44:10 +0200

    wip

--- aws/scripts/cleanup_ec2.sh
+++ aws/scripts/cleanup_ec2.sh
@@ -2,7 +2,7 @@
 
 cd "$(dirname "${0}")"
 
-go get github.com/taskcluster/generic-worker/cmd/aws-worker-types
-"$(go env GOPATH)/bin/aws-worker-types" > /dev/null
+go get github.com/taskcluster/generic-worker/aws/cmd/download-aws-worker-type-definitions
+"$(go env GOPATH)/bin/download-aws-worker-type-definitions" > /dev/null
 cat aws-provisioner-v1-worker-type-definitions/* | sed -n 's/^[[:space:]]*"ImageId": "//p' | sed -n 's/".*//p' | sort -u
 rm -r aws-provisioner-v1-worker-type-definitions
