4ea5bce556a0e9c1b72f6e8de9fc688f9d7500e0
Pete Moore <pmoore@mozilla.com>
2018-10-08 15:26:19 +0200

    fix

--- scripts/gotestcover.sh
+++ scripts/gotestcover.sh
@@ -5,14 +5,13 @@ if [ -z "${REPORT}" ]; then
   echo "Specify a report, e.g. '${0}' myreport.txt" >&2
   exit 64
 fi
-cd "$(dirname "${0}")"/..
 TEMP_SINGLE_REPORT="$(mktemp -t coverage.tmp.XXXXXXXXXX)"
 echo "mode: atomic" > "${REPORT}"
 HEAD_REV="$(git rev-parse HEAD)"
 # Dump package list to file rather than pipe, to avoid exit inside loop not
 # causing outer shell to exit due to running in a subshell.
 PACKAGE_LIST="$(mktemp -t package-list.tmp.XXXXXXXXXX)"
-go list ./... > "${PACKAGE_LIST}"
+go list ../... > "${PACKAGE_LIST}"
 
 while read package
 do
