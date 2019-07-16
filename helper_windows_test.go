6787581b4dd472fe602c796d5d4860b4fd4f2fea
Miles Crabill <mcrabill@mozilla.com>
2019-07-16 09:09:24 -0700

    rename env var to skip admin required tests to GW_SKIP_ADMIN_REQUIRED_TESTS

--- helper_windows_test.go
+++ helper_windows_test.go
@@ -119,6 +119,6 @@ func singleCommandNoArgs(command string) []string {
 
 // this is opt-out so that we don't skip tests by default
 func shouldRunAdminTests() bool {
-	_, ok := os.LookupEnv("SKIP_ADMINISTRATOR_TESTS")
+	_, ok := os.LookupEnv("GW_SKIP_SERVICE_INSTALLATION_TESTS")
 	return !ok
 }
