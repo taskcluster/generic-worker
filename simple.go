732575c799c9da9acc619dc83236f4d9c476a839
Miles Crabill <mcrabill@mozilla.com>
2019-07-18 16:41:38 -0700

    move generateCommand() to the correct simple.go

--- simple.go
+++ simple.go
@@ -2,7 +2,11 @@
 
 package main
 
-import "log"
+import (
+	"log"
+
+	"github.com/taskcluster/generic-worker/process"
+)
 
 const (
 	engine = "simple"
@@ -10,4 +14,16 @@ const (
 
 func secureConfigFile() {
 	log.Print("WARNING: can't secure generic-worker config file")
+}
+
+func (task *TaskRun) generateCommand(index int) error {
+	var err error
+	task.Commands[index], err = process.NewCommand(task.Payload.Command[index], taskContext.TaskDir, task.EnvVars())
+	if err != nil {
+		return err
+	}
+	task.logMux.RLock()
+	defer task.logMux.RUnlock()
+	task.Commands[index].DirectOutput(task.logWriter)
+	return nil
 }
