0f66c38ee323ce3919a7a171c93dcfa40cfc9224
Pete Moore <pmoore@mozilla.com>
2019-10-26 11:57:48 +0200

    try this

--- worker_types/generate_occ_userdata.sh
+++ worker_types/generate_occ_userdata.sh
@@ -5,4 +5,5 @@ echo "Removing..."
 rm -v gecko-*/bootstrap.ps1
 
 echo "Generating..."
-curl -L 'https://github.com/mozilla-releng/OpenCloudConfig/tree/master/userdata/Manifest' 2>/dev/null | sed -n 's/.*\(gecko[^.]*\)\.json.*/\1/p' | sort -u | xargs -n 1 -P 32 ./transform.sh
+# curl -L 'https://github.com/mozilla-releng/OpenCloudConfig/tree/master/userdata/Manifest' 2>/dev/null | sed -n 's/.*\(gecko[^.]*\)\.json.*/\1/p' | sort -u | xargs -n 1 -P 32 ./transform.sh
+curl -L 'https://github.com/mozilla-releng/OpenCloudConfig/tree/master/userdata/Manifest' 2>/dev/null | sed -n 's/.*\(gecko[^.]*\)\.json.*/\1/p' | sort -u | xargs -n 1 ./transform.sh
